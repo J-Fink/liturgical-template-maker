a250a078be500db8071a9235afe69994965e31bf
Joe Fink <57537040+J-Fink@users.noreply.github.com>
2021-10-03 21:23:55 -0500

    Updates

--- gatsby-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  pathPrefix: "/liturgical-template-maker",
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Liturgical Template Maker",
-  },
-  plugins: ["gatsby-plugin-styled-components"],
-};
