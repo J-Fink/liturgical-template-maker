a0e69865d05bb1622d10007912596e3351aa38a9
Joe Fink <57537040+J-Fink@users.noreply.github.com>
2021-10-08 05:37:38 -0500

    functional use of romcal library, calculate day of year of selected date

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,53 +1,82 @@
 import React, { useEffect, useRef, useState } from "react";
+import romcal from 'romcal';
+
+// function displayData(data) {
+  //   document.querySelector("pre").innerHTML = JSON.stringify(data, null, 2);
+  // }
+  export default function HomePage() {
+    const [litData, setLitData] = useState();
+    const [showLitData, setShowLitData] = useState(false);
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let month, day, year, currentSeason, baseURL, selectedDayOfYear, currentDayOfYear;
+    
+    //date is new Date(yyyy, m, d)
+    const dayOfYear = (date) => {
+      let selectedDate = date;
+      let startOfYearDate = new Date(selectedDate.getFullYear(), 0, 0);
+      let diff = (selectedDate - startOfYearDate) + ((startOfYearDate.getTimezoneOffset() - selectedDate.getTimezoneOffset()) * 60 * 1000);
+      let oneDay = 1000 * 60 * 60 * 24;
+      // let calculatedDay = Math.floor(diff / oneDay);
+      let dateCalculated = {
+        calculatedDay: Math.floor(diff / oneDay),
+      }
+      return(
+        dateCalculated
+      );
+      // console.log('Day of year: ' + calculatedDay);
+      
+      
+      // currentDayOfYear = Math.floor((date - new Date(date.getFullYear(), 0, 1)) / (1000*60*60*24));
+      // console.log(currentDayOfYear, 'line 15');
+    };
+
+    // var now = new Date();
+    // var start = new Date(now.getFullYear(), 0, 0);
+    // var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+    // var oneDay = 1000 * 60 * 60 * 24;
+    // var day = Math.floor(diff / oneDay);
+    // console.log('Day of year: ' + day);
 
 
 
-function displayData(data) {
-  document.querySelector("pre").innerHTML = JSON.stringify(data, null, 2);
-}
-export default function HomePage() {
-  const [litData, setLitData] = useState();
-  const [showLitData, setShowLitData] = useState(false);
-  let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  let month, day, year, currentSeason, baseURL;
+
+
+
   const dateRef = useRef(null);
-  // useEffect(() => {
-    //     setCurrentSeason(litData?.season[0].toUpperCase() + litData?.season.substring(1));
-    //   })
-    async function getMyData(URL) {
-      const res = await fetch(URL);
-      const data = await res.json();
-      setLitData(data)
-      console.log(litData);
-      // (data => {
-      // console.log(data, "line 18");
-      // console.log(litData, 'line 20');
-      // currentSeason = litData?.season[0].toUpperCase() + litData?.season.substring(1);
-      // console.log(currentSeason, "line 22");
-      // console.log(litData?.season[0].toUpperCase() + litData?.season.substring(1));
-    }
+
+  function getMyData(targetedDay) {
+    const exampleDay = romcal.calendarFor({
+      year: year,
+    })[targetedDay - 1];
+    // console.log(exampleDay[targetedDay - 1], 'line 25'); //subtract 1 to account for off by 1 error
+    console.log(exampleDay);
+  }
     
   
   const handleOnClick = () => {
-    // alert(litData.date);
-    setShowLitData(!showLitData);
+    // alert('hello');
+    let specificDayForCalendar = dayOfYear(new Date(year, month, day)).calculatedDay;
+    console.log(typeof specificDayForCalendar, specificDayForCalendar)
+    getMyData(specificDayForCalendar);
+    console.log(dayOfYear(new Date(year, month, day)).calculatedDay);
+    console.log(dayOfYear(new Date(year, month, day)));
+    
+    // setShowLitData(!showLitData);
     // {__html:'<li>hello man</li>'} if I want to set HTML
     // alert(litData, "litdata");
     // getMyData(baseURL).color;
   }
   const handleChange = () => {
-    // alert('changed!')
     //get mm/dd/yyyy from date input and set dateRef to that 
-    console.log(dateRef.current.value);
-    day = Math.floor(dateRef.current.value.split("-")[2]);
-    month = Math.floor(dateRef.current.value.split("-")[1]);
-    year = dateRef.current.value.split("-")[0];
-    baseURL = `http://calapi.inadiutorium.cz/api/v0/en/calendars/default/${year}/${month}/${day}`;
-    getMyData(baseURL);
-  console.log(month);
-  console.log(day);
-  console.log(year);
-  console.log(baseURL);
+    
+    day = parseInt(dateRef.current.value.split("-")[2]); //1-31
+    month = parseInt(dateRef.current.value.split("-")[1] - 1); //0-11
+    year = parseInt(dateRef.current.value.split("-")[0]);//yyyy
+    
+    console.log(month);
+    console.log(day);
+    console.log(year);
+  
   
  };
 
